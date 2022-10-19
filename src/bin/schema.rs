cfa8d5d736e00c6e002c6897b48eb937e101586f
Barton Rhodes <barton@bmorphism.xyz>
2022-10-18 22:49:37 -0700

    cw-template

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use risc0_cosmwasm_example::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
