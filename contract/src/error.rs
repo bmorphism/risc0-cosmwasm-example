83e17cd3e67df97c4059eaca58a292ed7054bef6
Barton Rhodes <barton@bmorphism.xyz>
2022-10-19 01:46:55 -0700

    begins multitest exec

--- contract/src/error.rs
+++ contract/src/error.rs
@@ -1,6 +1,10 @@
 use cosmwasm_std::StdError;
 use thiserror::Error;
 
+struct NgmiError{
+    
+}
+
 #[derive(Error, Debug)]
 pub enum ContractError {
     #[error("{0}")]
@@ -10,13 +14,13 @@ pub enum ContractError {
     Unauthorized {},
     // Add any other custom errors you like here.
     // Look at https://docs.rs/thiserror/1.0.21/thiserror/ for details.
-    #[error("Verification error")]
-    VerificationError {},
+    // #[error("Verification error")]
+    // VerificationError {},
 }
 
-impl From<risc0_zkvm_verify::zkp::verify::VerificationError> for ContractError {
-    fn from(
-        msg: risc0_zkvm_verify::zkp::verify::VerificationError,
-    ) -> ContractError::VerificationError {
-    }
-}
+// impl From<risc0_zkvm_verify::zkp::verify::VerificationError> for ContractError {
+//     fn from(
+//         msg: risc0_zkvm_verify::zkp::verify::VerificationError,
+//     ) -> ContractError::VerificationError {
+//     }
+// }
---
+++ contract/src/tests.rs
@@ -0,0 +1,7 @@
+use cw_multi_test::{App, Contract, ContractWrapper, Executor};
+use crate::msg::{ExectueMsg};
+
+#[test]
+fn test_verify() {
+    unimplemented!();
+}
